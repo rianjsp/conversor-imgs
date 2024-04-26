02eb684688de4ee28ee1395e18507fe274a8546f
rjzin <bigriann18@gmail.com>
2024-04-26 11:48:57 -0300

    Update 26/04
    Inicializando e finalizando a o arquivo js.
    Adicionando as funcoes de progressbar e startProcess ao sistema de conversao.

---
+++ static/main.js
@@ -0,0 +1,59 @@
+function progressBar() { 
+        var progressBar = document.getElementById('progressBar')
+        var width = 1;
+        var interval = setInterval(Frame, 10)
+
+        function Frame() { 
+            if (width >= 100) {
+                clearInterval(interval)
+            } else { 
+                width++;
+                progressBar.style.width = width + '%';  
+            }
+    }
+    
+}
+
+function startProcess() { 
+    progressBar()
+     
+    const file_ipt = document.getElementById('inputArq');
+    const out_ipt = document.getElementById('outArq').value;
+    const file = file_ipt.files[0];
+
+    if (!file) { 
+        alert('Por favor, selecione uma imagem para realizar a conversão!');
+        return; // Saia da função se nenhum arquivo foi selecionado
+    }
+
+    // Verifique se o arquivo é uma imagem
+    if (!file.type.startsWith('image/')) {
+        alert('Por favor, selecione um arquivo de imagem válido!');
+        return; // Saia da função se não for uma imagem
+    }
+    
+    const reader = new FileReader();
+    reader.onload = (e) => { 
+        const img = new Image();
+        img.onload = () => { 
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width; // Defina a largura do canvas
+            canvas.height = img.height; // Defina a altura do canvas
+            ctx.drawImage(img, 0, 0, img.width, img.height); // Inclua a largura e a altura do desenho
+
+            const convertedImg = canvas.toDataURL(`image/${out_ipt}`);
+
+            const d_link = document.createElement('a');
+            d_link.className = `linkOut`
+            d_link.href = convertedImg;
+            // Defina a extensão do arquivo de saída com base no tipo de imagem selecionada
+            d_link.download = `imagem_convertida.${out_ipt}`; 
+            d_link.click();
+        }
+
+        img.src = e.target.result;
+    }
+
+    reader.readAsDataURL(file);
+}
